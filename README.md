6b5b3d7406c55b9ecdb6500ba7accbf64d0de176
Razvan Cosma <razvan.cosma@digital.justice.gov.uk>
2018-03-27 15:38:51 +0100

    Terraform RDS

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # concourse-CP
 CI for Kubernetes cluster CP
+
+ * start with `AWS_DEFAULT_REGION='eu-west-1' terraform plan`
---
+++ terraform/main.tf
@@ -0,0 +1,20 @@
+resource "aws_db_instance" "default" {
+  depends_on             = ["aws_security_group.default"]
+  identifier             = "${var.identifier}"
+  allocated_storage      = "${var.storage}"
+  engine                 = "${var.engine}"
+  engine_version         = "${lookup(var.engine_version, var.engine)}"
+  instance_class         = "${var.instance_class}"
+  name                   = "${var.db_name}"
+  username               = "${var.username}"
+  password               = "${var.password}"
+  vpc_security_group_ids = ["${aws_security_group.default.id}"]
+  db_subnet_group_name   = "${aws_db_subnet_group.default.id}"
+  skip_final_snapshot    = true
+}
+
+resource "aws_db_subnet_group" "default" {
+  name        = "main_subnet_group"
+  description = "Our main group of subnets"
+  subnet_ids  = ["${var.subnet_1_id}", "${var.subnet_2_id}"]
+}
