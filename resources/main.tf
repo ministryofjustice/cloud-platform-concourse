be801028df102be8844bdb8c39127b1d2794b726
Dimitrios Karagiannis <dhkarag@gmail.com>
2018-05-29 15:51:12 +0100

    cleanup; merged tf files

--- resources/main.tf
+++ resources/main.tf
@@ -1,3 +1,33 @@
+resource "aws_security_group" "concourse" {
+  name        = "main_rds_sg"
+  description = "Allow all inbound traffic"
+  vpc_id      = "${var.vpc_id}"
+
+  ingress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "TCP"
+    cidr_blocks = ["${var.cidr_blocks}"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags {
+    Name = "${var.sg_name}"
+  }
+}
+
+resource "aws_db_subnet_group" "concourse" {
+  name        = "main_subnet_group"
+  description = "Our main group of subnets"
+  subnet_ids  = ["${var.subnet_1_id}", "${var.subnet_2_id}"]
+}
+
 resource "aws_db_instance" "concourse" {
   depends_on             = ["aws_security_group.concourse"]
   identifier             = "${var.identifier}"
@@ -11,10 +41,4 @@ resource "aws_db_instance" "concourse" {
   vpc_security_group_ids = ["${aws_security_group.concourse.id}"]
   db_subnet_group_name   = "${aws_db_subnet_group.concourse.id}"
   skip_final_snapshot    = true
-}
-
-resource "aws_db_subnet_group" "concourse" {
-  name        = "main_subnet_group"
-  description = "Our main group of subnets"
-  subnet_ids  = ["${var.subnet_1_id}", "${var.subnet_2_id}"]
 }
