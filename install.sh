6c4a4df4a6521ca4b01f7e8677716edadeb3523f
Dimitrios Karagiannis <dhkarag@gmail.com>
2018-05-30 18:25:55 +0100

    installation script

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+set -o pipefail
+set -o errexit
+
+usage() {
+  printf "${0} <cluster-name>\n"
+}
+
+if [ $# != 1 ]; then
+  usage
+  exit 1
+fi
+
+cd resources
+
+terraform init
+terraform workspace select "${1}"
+terraform apply -auto-approve
+
+helm \
+  --namespace concourse \
+  --name concourse \
+  -f ".helm-config/${1}/values.yaml" \
+  stable/concourse
