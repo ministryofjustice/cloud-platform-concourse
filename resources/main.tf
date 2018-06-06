bd1e6736acb58e116f70b8654b9e3faaea8dd327
john ojuolape <john.ojuolape@digital.justice.gov.uk>
2018-06-06 16:00:05 +0100

    added kubernetes secret object, access and secret key used as secret

--- resources/main.tf
+++ resources/main.tf
@@ -161,3 +161,15 @@ resource "aws_iam_policy_attachment" "attach-policy" {
   users      = ["${aws_iam_user.concourse-user.name}"]
   policy_arn = "${aws_iam_policy.policy.arn}"
 }
+
+resource "kubernetes_secret" "concourse_user_credentials"{
+  metadata {
+    name = "${terraform.workspace}-concourse-user"
+    namespace = "concourse-main"
+  }
+
+  data {
+    access_key_id  = "${aws_iam_access_key.iam_access_key.id}"
+    secret_access_key = "${aws_iam_access_key.iam_access_key.secret}"
+  }
+}
--- resources/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "AWS_IAM_ACCESS_KEY" {
-  description = "access key id for concourse user account"
-  value       = "${aws_iam_access_key.iam_access_key.id}"
-}
-
-output "AWS_IAM_SECRET_KEY" {
-  description = "secret key for concourse user account"
-  value       = "${aws_iam_access_key.iam_access_key.secret}"
-}
