8debc80c879293c843ddb4708363d6420e53bbe7
Razvan Cosma <razvan.cosma@digital.justice.gov.uk>
2019-05-30 13:29:22 +0100

    generate ECR via tf

---
+++ pipelines/live-1/main/tzcronjobber/ecr.tf
@@ -0,0 +1,26 @@
+resource "aws_ecr_repository" "repo" {
+  name = "cloud-platform/tzcronjobber"
+}
+
+resource "aws_ecr_lifecycle_policy" "lifecycle_policy" {
+  repository = "${aws_ecr_repository.repo.name}"
+
+  policy = <<EOF
+{
+    "rules": [
+        {
+            "rulePriority": 2,
+            "description": "Expire images over count 100",
+            "selection": {
+                "tagStatus": "any",
+                "countType": "imageCountMoreThan",
+                "countNumber": 100
+            },
+            "action": {
+                "type": "expire"
+            }
+        }
+    ]
+}
+EOF
+}
