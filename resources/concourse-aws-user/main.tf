367f3720259a48e67164d4d87cc6e846d0690ae5
vijay-veeranki-moj <vijay.veeranki@digital.justice.gov.uk>
2020-06-08 16:43:29 +0100

    Add "acm:DeleteCertificate"
    For concourse user to run destroy cluster

--- resources/concourse-aws-user/main.tf
+++ resources/concourse-aws-user/main.tf
@@ -116,6 +116,7 @@ data "aws_iam_policy_document" "policy" {
     actions = [
       "ec2:*",
       "acm:RequestCertificate",
+      "acm:DeleteCertificate",
 
       "iam:CreateRole",
       "iam:AttachRolePolicy",
