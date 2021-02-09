3bf8749c1ebc5114a5c128c35334e143173a7ca4
Alejandro Garrido Mota <garridomota@gmail.com>
2021-02-09 15:38:46 +0000

    Initial mock

--- pipelines/manager/main/build-environments/main.tf
+++ pipelines/manager/main/build-environments/main.tf
@@ -15,3 +15,31 @@ provider "aws" {
   region = "eu-west-1"
 }
 
+provider "kubernetes_live_1" {
+  config_path    = "~/.kube/config"
+  config_context = "live-1"
+}
+
+provider "kubernetes_manager" {
+  config_path    = "~/.kube/config"
+  config_context = "manager"
+}
+
+resource "kubernetes_service_account" "manager" {
+  metadata {
+    name = "terraform-example"
+  }
+  secret {
+    name = "${kubernetes_secret.example.metadata.0.name}"
+  }
+}
+
+resource "kubernetes_service_account" "live_1" {
+  metadata {
+    name = "terraform-example"
+  }
+  secret {
+    name = "${kubernetes_secret.example.metadata.0.name}"
+  }
+}
+
