7e78a4b21b481454bf85551aeae57a28ec38417a
poornima-moj <poornima.krishnasamy@digital.justice.gov.uk>
2021-05-11 12:11:55 +0100

    Remove seerviceaccount creation for live-1 and manager. Duplicate. For manager, it is part of terraform-concourse module. For live-1, it is defined in kube-system namespace in env repo

--- pipelines/manager/main/build-environments/main.tf
+++ pipelines/manager/main/build-environments/main.tf
@@ -16,35 +16,4 @@ provider "aws" {
 }
 
 
-provider "kubernetes" {
-  alias          = "live-1"
-  config_context = "live-1.cloud-platform.service.justice.gov.uk"
-}
-
-provider "kubernetes" {
-  alias          = "manager"
-  config_context = "arn:aws:eks:eu-west-2:754256621582:cluster/manager"
-}
-
-resource "kubernetes_service_account" "manager" {
-  metadata {
-    name      = "concourse-build-environments"
-    namespace = "kube-system"
-  }
-
-  automount_service_account_token = false
-
-  provider = kubernetes.manager
-}
-
-resource "kubernetes_service_account" "live_1" {
-  metadata {
-    name      = "concourse-build-environments"
-    namespace = "kube-system"
-  }
-
-  automount_service_account_token = false
-
-  provider = kubernetes.live-1
-}
 
